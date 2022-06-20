ca6506ce0eae975b4c74a952e3b7230f5627e9af
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-20 11:52:51 -0600

    Began styling the website

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,19 +9,28 @@ export default class NavigationContainer extends Component {
     render(){
         return(
             <div>
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                    Home
-                </NavLink>
-                <NavLink exact to="/about-me" activeClassName="nav-link-active">
-                    About
-                </NavLink>
-                <NavLink exact to="/contact" activeClassName="nav-link-active">
-                    Contact
-                </NavLink>
-                <NavLink exact to="/blog" activeClassName="nav-link-active"s>
-                    Blog
-                </NavLink>
-                {false ? <button>Add Blog</button> : null}
+                <div className="nav-wrapper">
+                    <div className="left-side">
+                        <NavLink exact to="/" activeClassName="nav-link-active">
+                            Home
+                        </NavLink>
+                        <NavLink exact to="/about-me" activeClassName="nav-link-active">
+                            About
+                        </NavLink>
+                        <NavLink exact to="/contact" activeClassName="nav-link-active">
+                            Contact
+                        </NavLink>
+                        <NavLink exact to="/blog" activeClassName="nav-link-active"s>
+                            Blog
+                        </NavLink>  
+                         {false ? <button>Add Blog</button> : null}
+                    </div>
+                    <div className="right-side">
+                        JORDEN DICKERSON
+                    </div>
+                </div>
+                
+                
             </div>
         );
     }
