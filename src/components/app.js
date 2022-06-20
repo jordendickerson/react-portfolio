dc45715abf0a2f383ac971bc13a424cf65e82e60
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-20 08:52:48 -0600

    Added nav links

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 import PortfolioContainer from "./portfolio/portfolio-container.js";
 import NavigationContainer from "./navigation/navigation-container.js";
+import Home from "./pages/home.js";
+import About from "./pages/about.js";
+import Contact from "./pages/contact.js";
+import Blog from "./pages/blog.js";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
+
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Jorden Dickerson Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
 
 export default class NavigationContainer extends Component {
     constructor(){
@@ -8,11 +9,19 @@ export default class NavigationContainer extends Component {
     render(){
         return(
             <div>
-                <button>Home</button>
-                <button>About</button>
-                <button>Contact</button>
-                <button>Blog</button>
-                {true ? <button>Add Blog</button> : null}
+                <NavLink exact to="/" activeClassName="nav-link-active">
+                    Home
+                </NavLink>
+                <NavLink exact to="/about-me" activeClassName="nav-link-active">
+                    About
+                </NavLink>
+                <NavLink exact to="/contact" activeClassName="nav-link-active">
+                    Contact
+                </NavLink>
+                <NavLink exact to="/blog" activeClassName="nav-link-active"s>
+                    Blog
+                </NavLink>
+                {false ? <button>Add Blog</button> : null}
             </div>
         );
     }
