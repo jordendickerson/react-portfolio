3d93f3425960a40fa776835d77a893fe7fb66fac
Jorden Dickerson <jordendickerson24@gmail.com>
2022-06-20 06:56:34 -0600

    Navigation Bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import moment from "moment";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
+import PortfolioContainer from "./portfolio/portfolio-container.js";
+import NavigationContainer from "./navigation/navigation-container.js";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Jorden Dickerson Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
