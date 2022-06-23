759892d1c8a38c521d8d702ada3aca7afb4b3ebd
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-23 09:46:03 -0600

    Created portfolio manager sidebar

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,19 +1,50 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 
-export default class PortfolioManager extends Component{
-    render( ){
-        return(
-            <div>
-                <div className="form-sidebar-wrapper">
-                    <div className="form-wrapper">
-                        <h1>PORTFOLIO FORM</h1>
-                    </div>
-                    <div className="sidebar-wrapper">
-                        <h1>PORTFOLIO SIDEBAR</h1>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordendickerson.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      }) //calls the portfolio items from the API
+      .then((response) => {
+        //sets the data state to the portfolio items from the API
+        console.log('response from portfolioitems', response);
+        this.setState({
+          data: [...response.data.portfolio_items]
+        }).catch(error => {
+          console.log("error in getPortfolioItems:", error);
+        });
+      });
+  }
+
+  //when this page loads in the app, componentDidMount executes
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="form-sidebar-wrapper">
+          <div className="form-wrapper">
+            <h1>PORTFOLIO FORM</h1>
+          </div>
+          <div className="sidebar-wrapper">
+            <PortfolioSidebarList data={this.state.data}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
