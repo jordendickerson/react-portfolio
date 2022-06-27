119a5b830b8dfb6b1b9b0dbfcee006a53fea4c79
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-27 09:17:24 -0600

    Added banner and logo image drops to portfolio manager

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -25,14 +25,32 @@ export default class PortfolioForm extends Component {
     this.componentConfig = this.componentConfig.bind(this);
     this.djsConfig = this.djsConfig.bind(this);
     this.handleThumbDrop = this.handleThumbDrop.bind(this);
+    this.handleBannerDrop = this.handleBannerDrop.bind(this);
+    this.handleLogoDrop = this.handleLogoDrop.bind(this);
+
+    this.thumbRef = React.createRef();
+    this.bannerRef = React.createRef();
+    this.logoRef = React.createRef();
   }
 
   handleThumbDrop() {
-    return{
-        addedfile: file => this.setState({ thumb_image: file })
+    return {
+      addedfile: (file) => this.setState({ thumb_image: file }),
     };
   }
-  
+
+  handleBannerDrop() {
+    return {
+      addedfile: (file) => this.setState({ banner_image: file }),
+    };
+  }
+
+  handleLogoDrop() {
+    return {
+      addedfile: (file) => this.setState({ logo: file }),
+    };
+  }
+
   componentConfig() {
     return {
       iconFiletypes: [".jpg", ".png"],
@@ -58,7 +76,15 @@ export default class PortfolioForm extends Component {
     formData.append("portfolio_item[position]", this.state.position);
 
     if (this.state.thumb_image) {
-        formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
+
+    if (this.state.banner_image) {
+      formData.append("portfolio_item[banner_image]", this.state.banner_image);
+    }
+
+    if (this.state.logo) {
+      formData.append("portfolio_item[logo]", this.state.logo);
     }
 
     return formData;
@@ -79,6 +105,21 @@ export default class PortfolioForm extends Component {
       )
       .then((response) => {
         this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
+
+        this.setState({
+          name: "",
+          description: "",
+          url: "",
+          category: "eCommerce",
+          position: "",
+          thumb_image: "",
+          banner_image: "",
+          logo: "",
+        });
+
+        [this.thumbRef, this.bannerRef, this.logoRef].forEach((ref) => {
+          ref.current.dropzone.removeAllFiles();
+        });
       })
       .catch((error) => {
         console.log("Error in portfolio form handleSubmit", error);
@@ -88,10 +129,7 @@ export default class PortfolioForm extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Portfolio Form</h1>
-
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
           <div>
             <input
               type="text"
@@ -139,9 +177,22 @@ export default class PortfolioForm extends Component {
 
           <div className="image-uploaders">
             <DropzoneComponent
+              ref={this.thumbRef}
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleThumbDrop()}
+            />
+            <DropzoneComponent
+              ref={this.bannerRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleBannerDrop()}
+            />
+            <DropzoneComponent
+              ref={this.logoRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleLogoDrop()}
             />
           </div>
 
@@ -149,7 +200,6 @@ export default class PortfolioForm extends Component {
             <button type="submit">Save</button>
           </div>
         </form>
-      </div>
     );
   }
 }
