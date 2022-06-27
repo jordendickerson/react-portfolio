76c2dfc102894c97a66ddaf7636eca213db67d5b
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-27 10:06:13 -0600

    Created portfolio item delete function in portfolio manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -12,30 +12,52 @@ export default class PortfolioManager extends Component {
       data: [],
     };
 
-    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    this.handleSuccessfulFormSubmission =
+      this.handleSuccessfulFormSubmission.bind(this);
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick(portfolioItem) {
+    axios.delete(
+      `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+      { withCredentials: true }
+    ).then(response => {
+      this.setState({
+        data: this.state.data.filter(item => {
+          return item.id !== portfolioItem.id;
+        })
+      })
+
+      return response.data;
+    }).catch(error => {
+      console.log("Error from handleDeleteClick", error);
+    })
   }
 
   handleSuccessfulFormSubmission(portfolioItem) {
     this.setState({
-      data: [portfolioItem].concat(this.state.data)
-    })
+      data: [portfolioItem].concat(this.state.data),
+    });
   }
 
   handleFormSubmissionError(error) {
-    console.log("handleFormSubmissionError error", error)
+    console.log("handleFormSubmissionError error", error);
   }
 
   getPortfolioItems() {
     axios
-      .get("https://jordendickerson.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {
-        withCredentials: true,
-      }) //calls the portfolio items from the API
+      .get(
+        "https://jordendickerson.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true,
+        }
+      ) //calls the portfolio items from the API
       .then((response) => {
         //sets the data state to the portfolio items from the API
         this.setState({
-          data: [...response.data.portfolio_items]
-        }).catch(error => {
+          data: [...response.data.portfolio_items],
+        }).catch((error) => {
           console.log("error in getPortfolioItems:", error);
         });
       });
@@ -51,13 +73,18 @@ export default class PortfolioManager extends Component {
       <div>
         <div className="form-sidebar-wrapper">
           <div className="form-wrapper">
-            <PortfolioForm 
-              handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            <PortfolioForm
+              handleSuccessfulFormSubmission={
+                this.handleSuccessfulFormSubmission
+              }
               handleFormSubmissionError={this.handleFormSubmissionError}
             />
           </div>
           <div className="sidebar-wrapper">
-            <PortfolioSidebarList data={this.state.data}/>
+            <PortfolioSidebarList
+              handleDeleteClick={this.handleDeleteClick}
+              data={this.state.data}
+            />
           </div>
         </div>
       </div>
