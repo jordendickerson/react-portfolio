25dbcf2c3c786b9c6ab92c5504da67c1b3723094
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-28 09:10:53 -0600

    Added image editing in portfolio manager

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faTrash, faSignOutAlt, faEdit} from "@fortawesome/free-solid-svg-icons"
+import {faTrash, faSignOutAlt, faEdit, faBan} from "@fortawesome/free-solid-svg-icons"
 
 //PAGE IMPORTS
 import PortfolioContainer from "./portfolio/portfolio-container.js";
@@ -17,7 +17,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail.js";
 import Auth from "./pages/auth.js";
 import NoMatch from "./pages/no-match.js";
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faBan);
 
 export default class App extends Component {
   constructor(props) {
--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { DropzoneComponent } from "react-dropzone-component";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
 import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
@@ -20,7 +23,7 @@ export default class PortfolioForm extends Component {
       logo: "",
       editMode: false,
       apiUrl: "https://jordendickerson.devcamp.space/portfolio/portfolio_items",
-      apiAction: "post"
+      apiAction: "post",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -30,14 +33,30 @@ export default class PortfolioForm extends Component {
     this.handleThumbDrop = this.handleThumbDrop.bind(this);
     this.handleBannerDrop = this.handleBannerDrop.bind(this);
     this.handleLogoDrop = this.handleLogoDrop.bind(this);
+    this.deleteImage = this.deleteImage.bind(this);
 
     this.thumbRef = React.createRef();
     this.bannerRef = React.createRef();
     this.logoRef = React.createRef();
   }
 
+  deleteImage(imageType) {
+    axios.delete(
+      `https://api.devcamp.space/portfolio/delete-portfolio-image/${this.state
+        .id}?image_type=${imageType}`,
+      { withCredentials: true }
+    )
+    .then(response => {
+      this.setState({
+        [`${imageType}_url`]: ""
+      });
+    }).catch(error => {
+      console.log("deleteImage error", error);
+    })
+  }
+
   componentDidUpdate() {
-    if (Object.keys(this.props.portfolioToEdit).length > 0){
+    if (Object.keys(this.props.portfolioToEdit).length > 0) {
       const {
         id,
         name,
@@ -47,7 +66,7 @@ export default class PortfolioForm extends Component {
         url,
         thumb_image_url,
         banner_image_url,
-        logo_url
+        logo_url,
       } = this.props.portfolioToEdit;
 
       this.props.clearPortfolioToEdit();
@@ -61,8 +80,11 @@ export default class PortfolioForm extends Component {
         position: position || "",
         editMode: true,
         apiUrl: `https://jordendickerson.devcamp.space/portfolio/portfolio_items/${id}`,
-        apiAction: "patch"
-      })
+        apiAction: "patch",
+        thumb_image_url: thumb_image_url || "",
+        banner_image_url: banner_image_url || "",
+        logo_url: logo_url || "",
+      });
     }
   }
 
@@ -134,7 +156,7 @@ export default class PortfolioForm extends Component {
       method: this.state.apiAction,
       url: this.state.apiUrl,
       data: this.buildForm(),
-      withCredentials: true
+      withCredentials: true,
     })
       .then((response) => {
         if (this.state.editMode) {
@@ -153,8 +175,9 @@ export default class PortfolioForm extends Component {
           banner_image: "",
           logo: "",
           editMode: false,
-          apiUrl: "https://jordendickerson.devcamp.space/portfolio/portfolio_items",
-          apiAction: "post"
+          apiUrl:
+            "https://jordendickerson.devcamp.space/portfolio/portfolio_items",
+          apiAction: "post",
         });
 
         [this.thumbRef, this.bannerRef, this.logoRef].forEach((ref) => {
@@ -169,54 +192,65 @@ export default class PortfolioForm extends Component {
 
   render() {
     return (
-        <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
-          <div className="two-column">
-            <input
-              type="text"
-              name="name"
-              placeholder="Portfolio Item name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              name="url"
-              placeholder="URL"
-              value={this.state.url}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="two-column">
-            <input
-              type="text"
-              name="position"
-              placeholder="Position"
-              value={this.state.position}
-              onChange={this.handleChange}
-            />
-            <select
-              name="category"
-              value={this.state.category}
-              onChange={this.handleChange}
-              className="select-element"
-            >
-              <option value="eCommerce">eCommerce</option>
-              <option value="Scheduling">Scheduling</option>
-              <option value="Enterprise">Enterprise</option>
-              <option value="Technology">Technology</option>
-            </select>
-          </div>
-          <div className="one-column">
-            <textarea
-              type="text"
-              name="description"
-              placeholder="Description"
-              value={this.state.description}
-              onChange={this.handleChange}
-            />
-          </div>
+      <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
+        <div className="two-column">
+          <input
+            type="text"
+            name="name"
+            placeholder="Portfolio Item name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="url"
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="two-column">
+          <input
+            type="text"
+            name="position"
+            placeholder="Position"
+            value={this.state.position}
+            onChange={this.handleChange}
+          />
+          <select
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+            className="select-element"
+          >
+            <option value="eCommerce">eCommerce</option>
+            <option value="Scheduling">Scheduling</option>
+            <option value="Enterprise">Enterprise</option>
+            <option value="Technology">Technology</option>
+          </select>
+        </div>
+        <div className="one-column">
+          <textarea
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
 
-          <div className="image-uploaders">
+        <div className="image-uploaders">
+          {this.state.thumb_image_url && this.state.editMode ? (
+            <div className="portfolio-manager-image-wrapper">
+              <img src={this.state.thumb_image_url} />
+
+              <div className="image-removal-link">
+                <a onClick={() => this.deleteImage("thumb_image")}>
+                  <FontAwesomeIcon icon="ban" />
+                </a>
+              </div>
+            </div>
+          ) : (
             <DropzoneComponent
               ref={this.thumbRef}
               config={this.componentConfig()}
@@ -225,6 +259,19 @@ export default class PortfolioForm extends Component {
             >
               <div className="dz-message">Thumbnail</div>
             </DropzoneComponent>
+          )}
+
+          {this.state.banner_image_url && this.state.editMode ? (
+            <div className="portfolio-manager-image-wrapper">
+              <img src={this.state.banner_image_url} />
+
+              <div className="image-removal-link">
+                <a onClick={() => this.deleteImage("banner_image")}>
+                  <FontAwesomeIcon icon="ban" />
+                </a>
+              </div>
+            </div>
+          ) : (
             <DropzoneComponent
               ref={this.bannerRef}
               config={this.componentConfig()}
@@ -233,6 +280,19 @@ export default class PortfolioForm extends Component {
             >
               <div className="dz-message">Banner</div>
             </DropzoneComponent>
+          )}
+
+          {this.state.logo_url && this.state.editMode ? (
+            <div className="portfolio-manager-image-wrapper">
+              <img src={this.state.logo_url} />
+
+              <div className="image-removal-link">
+                <a onClick={() => this.deleteImage("logo")}>
+                  <FontAwesomeIcon icon="ban" />
+                </a>
+              </div>
+            </div>
+          ) : (
             <DropzoneComponent
               ref={this.logoRef}
               config={this.componentConfig()}
@@ -241,12 +301,15 @@ export default class PortfolioForm extends Component {
             >
               <div className="dz-message">Logo</div>
             </DropzoneComponent>
-          </div>
+          )}
+        </div>
 
-          <div>
-            <button className="btn" type="submit">Save</button>
-          </div>
-        </form>
+        <div>
+          <button className="btn" type="submit">
+            Save
+          </button>
+        </div>
+      </form>
     );
   }
 }
