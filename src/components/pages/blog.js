bc3057859abfb5f90417223773d1791def87e0b9
Jorden Dickerson <jorden.dickerson@tooeleschools.org>
2022-06-29 09:39:20 -0600

    Created blog modal

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BlogModal from "../modals/blog-modal.js";
 
 import BlogItem from "../blog/blog-item";
 
@@ -14,28 +15,42 @@ class Blog extends Component {
       totalCount: 0,
       currentPage: 0,
       isLoading: true,
+      showModal: false,
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
   }
 
-  activateInfiniteScroll() {
-    window.onscroll = () => {
-      if (
-        this.state.isLoading ||
-        this.state.blogItems.length === this.state.totalCount
-      ) {
-        return;
-      }
+  handleOpenModal() {
+    this.setState({
+      showModal: true,
+    });
+  }
 
-      if (
-        window.innerHeight + document.documentElement.scrollTop >=
-        document.documentElement.offsetHeight
-      ) {
-        this.getBlogItems();
-      }
-    };
+  handleCloseModal() {
+    this.setState({
+      showModal: false,
+    });
+  }
+
+  onScroll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
+    }
   }
 
   getBlogItems() {
@@ -65,6 +80,10 @@ class Blog extends Component {
     this.getBlogItems();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
   render() {
     const blogRecords = this.state.blogItems.map((blogItem) => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
@@ -72,6 +91,13 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
+        <div className="new-blog-link">
+          <a onClick={this.handleOpenModal}>OPEN MODAL</a>
+        </div>
+        <BlogModal
+          showModal={this.state.showModal}
+          handleCloseModal={this.handleCloseModal}
+        />
         <div className="content-container">{blogRecords}</div>
         {this.state.isLoading ? (
           <div className="content-loader">
